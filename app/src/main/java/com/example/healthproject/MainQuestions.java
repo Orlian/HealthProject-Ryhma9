94140408d2784530098e9edd6882bae457e078bc
Joonas88 <56264853+Joonas88@users.noreply.github.com>
2020-02-26 11:20:26 +0200

    Ensimmäiseen slideriin lisätty kuuntelu

--- app/src/main/java/com/example/healthproject/MainQuestions.java
+++ app/src/main/java/com/example/healthproject/MainQuestions.java
@@ -3,6 +3,8 @@ package com.example.healthproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainQuestions extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class MainQuestions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_questions);
+
+        SeekBar seekBar = findViewById(R.id.seekBar2);
+        if (seekBar!=null){
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    progress = progress +1;
+                    Toast.makeText(getApplicationContext(),"seekbar progress: " +progress, Toast.LENGTH_SHORT).show();
+
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    Toast.makeText(getApplicationContext(),"seekbar touch started!", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    Toast.makeText(getApplicationContext(),"seekbar touch stopped!", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
     }
 }
