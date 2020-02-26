a4342a7323843ba67f71af7f6702913c7f71c0a2
Joonas88 <56264853+Joonas88@users.noreply.github.com>
2020-02-26 15:38:40 +0200

    Kysymysten ensimmäinen switch-case luotu, joka tuottaa tulosta

--- app/src/main/java/com/example/healthproject/MainQuestions.java
+++ app/src/main/java/com/example/healthproject/MainQuestions.java
@@ -2,7 +2,11 @@ package com.example.healthproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Toast;
 /**
@@ -11,13 +15,47 @@ import android.widget.Toast;
  * @version 1.0
  */
 public class MainQuestions extends AppCompatActivity {
+    int answer1=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_questions);
 
-
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.question1);
+        int chekedRadioButtonID = radioGroup.getCheckedRadioButtonId();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int checkedId) {
+                switch (checkedId) {
+                    default:
+                        Log.v("DEBUG", "Ei mitteen");
+                        break;
+                    case R.id.rb11:
+                        Log.v("DEBUG", "Ykkönen");
+                        answer1=1;
+                        break;
+                    case R.id.rb12:
+                        Log.v("DEBUG", "Kakkonen");
+                        answer1=2;
+                        break;
+                    case R.id.rb13:
+                        Log.v("DEBUG", "Kolmonen");
+                        answer1=3;
+                        break;
+                    case R.id.rb14:
+                        Log.v("DEBUG", "Nelonen");
+                        answer1=4;
+                        break;
+                    case R.id.rb15:
+                        Log.v("DEBUG", "Vitonen");
+                        answer1=5;
+                        break;
+                }
+            }
+        });
 
     }
-}
+
+    }
+
