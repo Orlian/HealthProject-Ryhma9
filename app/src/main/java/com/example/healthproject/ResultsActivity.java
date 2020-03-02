624b1f14d4621c4774ef57307ec534874af5eaa6
35840 <osku1993@luukku.com>
2020-03-02 16:11:00 +0200

    nimesin yhteenveto layoutin teksti id:t ja korjasin yhteenvedon koodia

--- app/src/main/java/com/example/healthproject/ResultsActivity.java
+++ app/src/main/java/com/example/healthproject/ResultsActivity.java
@@ -21,76 +21,76 @@ public class ResultsActivity extends AppCompatActivity {
         int groupAverage = intent.getIntExtra(QuestionActivity.EXTRA_GROUP_AVERAGE);
 
         if(group1<5){
-          TextView textView1 = (TextView) findViewById(R.id.textView1);
+          TextView textView1 = (TextView) findViewById(R.id.resultView1);
           textView1.setText(kato value);
         } else if(group1<10){
-          TextView textView2 = (TextView) findViewById(R.id.textView2);
-          textView2.setText(kato value);
+          TextView textView2 = (TextView) findViewById(R.id.resultView1);
+          textView1.setText(kato value);
         } else if(group1<15){
-          TextView textView3 = (TextView) findViewById(R.id.textView3);
-          textView3.setText(kato value);
+          TextView textView3 = (TextView) findViewById(R.id.resultView1);
+          textView1.setText(kato value);
           } else {
-           TextView textView4 = (TextView) findViewById(R.id.textView4);
-           textView4.setText(katovalue);
+           TextView textView4 = (TextView) findViewById(R.id.resultView1);
+           textView1.setText(katovalue);
           }
         }
 
          if(group2<5){
-         TextView textView1 = (TextView) findViewById(R.id.textView1);
+         TextView textView5 = (TextView) findViewById(R.id.resultView2);
          textView1.setText(kato value);
          } else if(group2<10){
-         TextView textView2 = (TextView) findViewById(R.id.textView2);
+         TextView textView6 = (TextView) findViewById(R.id.resultView2);
          textView2.setText(kato value);
          } else if(group2<15){
-         TextView textView3 = (TextView) findViewById(R.id.textView3);
+         TextView textView7 = (TextView) findViewById(R.id.resultView2);
          textView3.setText(kato value);
          } else {
-         TextView textView4 = (TextView) findViewById(R.id.textView4);
+         TextView textView8 = (TextView) findViewById(R.id.resultView2);
          textView4.setText(katovalue);
          }
          }
 
          if(group3<5){
-         TextView textView1 = (TextView) findViewById(R.id.textView1);
+         TextView textView9 = (TextView) findViewById(R.id.resultView3);
          textView1.setText(kato value);
          } else if(group3<10){
-         TextView textView2 = (TextView) findViewById(R.id.textView2);
+         TextView textView10 = (TextView) findViewById(R.id.resultView3);
          textView2.setText(kato value);
          } else if(group3<15){
-         TextView textView3 = (TextView) findViewById(R.id.textView3);
+         TextView textView11 = (TextView) findViewById(R.id.resultView3);
          textView3.setText(kato value);
          } else {
-         TextView textView4 = (TextView) findViewById(R.id.textView4);
+         TextView textView12 = (TextView) findViewById(R.id.resultView3);
          textView4.setText(katovalue);
          }
          }
 
          if(group4<5){
-         TextView textView1 = (TextView) findViewById(R.id.textView1);
+         TextView textView13 = (TextView) findViewById(R.id.resultView4);
          textView1.setText(kato value);
          } else if(group4<10){
-         TextView textView2 = (TextView) findViewById(R.id.textView2);
+         TextView textView14 = (TextView) findViewById(R.id.resultView4);
          textView2.setText(kato value);
          } else if(group4<15){
-         TextView textView3 = (TextView) findViewById(R.id.textView3);
+         TextView textView15 = (TextView) findViewById(R.id.resultView4);
          textView3.setText(kato value);
          } else {
-         TextView textView4 = (TextView) findViewById(R.id.textView4);
+         TextView textView16 = (TextView) findViewById(R.id.resultView4);
          textView4.setText(katovalue);
          }
          }
 
          if(groupAverage<5){
-         TextView textView1 = (TextView) findViewById(R.id.textView1);
+         TextView textView17 = (TextView) findViewById(R.id.resultAverage);
          textView1.setText(kato value);
          } else if(groupAverage<10){
-         TextView textView2 = (TextView) findViewById(R.id.textView2);
+         TextView textView18 = (TextView) findViewById(R.id.resultAverage);
          textView2.setText(kato value);
          } else if(groupAverage<15){
-         TextView textView3 = (TextView) findViewById(R.id.textView3);
+         TextView textView19 = (TextView) findViewById(R.id.resultAverage);
          textView3.setText(kato value);
          } else {
-         TextView textView4 = (TextView) findViewById(R.id.textView4);
+         TextView textView20 = (TextView) findViewById(R.id.resultAverage);
          textView4.setText(katovalue);
          }
          }
