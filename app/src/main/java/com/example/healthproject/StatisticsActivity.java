e2d1631959d250671e9394cc7b6357fa39573a6e
Joonas88 <56264853+Joonas88@users.noreply.github.com>
2020-03-09 16:59:25 +0200

    StatisticsActivityyn koitettu luoda arvoja ja ei toimi vielä. KESKEN

--- app/src/main/java/com/example/healthproject/StatisticsActivity.java
+++ app/src/main/java/com/example/healthproject/StatisticsActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
 import com.anychart.APIlib;
 import com.anychart.AnyChart;
@@ -15,9 +14,13 @@ import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.charts.Pie;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.example.healthproject.QuestionActivity.EXTRA_GROUP1;
 import static com.example.healthproject.QuestionActivity.EXTRA_GROUP2;
@@ -33,7 +36,9 @@ import static com.example.healthproject.QuestionActivity.EXTRA_GROUP_AVERAGE;
 public class StatisticsActivity extends AppCompatActivity {
     AnyChartView anyChartView;
     AnyChartView anyChartView2;
-    private SharedPreferences resultPrefs;
+    private SharedPreferences settings, resultPref;
+    private User testUser;
+    private HashMap <User, int[]> dataBank;
 
     /*
     switch komentoon liittyvät muuttujat.
@@ -56,6 +61,63 @@ public class StatisticsActivity extends AppCompatActivity {
         APIlib.getInstance().setActiveAnyChartView(anyChartView2);
         setupChart2();
 
+    }
+    protected void onStop(){
+        super.onStop();
+
+        Gson gson = new Gson();
+        Type gsonType = new TypeToken<HashMap>() {}.getType();
+
+        SharedPreferences settings = getSharedPreferences("LOGIN_PREF",MODE_PRIVATE);
+        String gsonString = gson.toJson(dataBank,gsonType);
+        String json = settings.getString("ACTIVE_USER", " ");
+        testUser = gson.fromJson(json, User.class);
+        SharedPreferences.Editor editor = settings.edit();
+        Log.v("DEBUG9","TESTUSER: "+testUser);
+
+
+        Intent intent = getIntent();
+        int group1 = intent.getIntExtra(EXTRA_GROUP1, 0);
+        int group2 = intent.getIntExtra(EXTRA_GROUP2, 0);
+        int group3 = intent.getIntExtra(EXTRA_GROUP3, 0);
+        int group4 = intent.getIntExtra(EXTRA_GROUP4, 0);
+        int groupAverage = intent.getIntExtra(EXTRA_GROUP_AVERAGE, 0);
+
+        int answersGroups[] ={group1,group2,group3,group4,groupAverage};
+
+        dataBank = new Gson().fromJson(gsonString, new TypeToken<HashMap<Object, int[] >>() {}.getType());
+
+        if (dataBank.containsKey(testUser)) {
+
+                dataBank.put(testUser, answersGroups);
+                Log.v("DEBUG9", "Lisätään dataBank:n arvoja if: "+dataBank);
+
+        } else if(dataBank != null) {
+
+            dataBank.put(testUser, answersGroups);
+            Log.v("DEBUG9", "Lisätään dataBank:n arvoja else if: "+dataBank);
+
+        } else {
+
+            dataBank = new HashMap<>();
+            dataBank.put(testUser, answersGroups);
+            Log.v("DEBUG9", "Lisätään dataBank:n arvoja else: "+dataBank);
+        }
+        /* EI TOIMI VIELÄ KUNNOLLA!*/
+
+        resultPref = getSharedPreferences("RESULTS_PREFS", MODE_PRIVATE);
+        SharedPreferences.Editor resultsEdit = resultPref.edit();
+        gsonType = new TypeToken<HashMap>() {}.getType();
+        gsonString = gson.toJson(dataBank,gsonType);
+        resultsEdit.clear();
+        resultsEdit.putString("DATABANK_VALUE", gsonString);
+        resultsEdit.commit();
+        Log.v("DEBUG0","GSON HASHMAP: "+gsonString);
+
+        // Necessary to clear first if we save preferences onPause.
+
+        //editor.("DATABANK_VALUES", );
+        //editor.commit();
     }
 
     public void setupChart1() {
@@ -92,6 +154,12 @@ public class StatisticsActivity extends AppCompatActivity {
         String[] states = {getString(R.string.group1head), getString(R.string.group2head), getString(R.string.group3head), getString(R.string.group4head)};
         Pie pie2 = AnyChart.pie();
         List<DataEntry> dataEntries2 = new ArrayList<>();
+        Intent intent = getIntent();
+        int group1 = intent.getIntExtra(EXTRA_GROUP1, 0);
+        int group2 = intent.getIntExtra(EXTRA_GROUP2, 0);
+        int group3 = intent.getIntExtra(EXTRA_GROUP3, 0);
+        int group4 = intent.getIntExtra(EXTRA_GROUP4, 0);
+        int groupAverage = intent.getIntExtra(EXTRA_GROUP_AVERAGE, 0);
         int[] values2 = {100, 10, 10, 10};
         for (int i = 0; i < states.length; i++) {
             dataEntries2.add(new ValueDataEntry(states[i], values2[i]));
